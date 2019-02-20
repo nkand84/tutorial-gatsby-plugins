9c74e0651d68069f3bf9a21433f7068bd1361530
nkand84 <nityabhagi@gmail.com>
2019-02-20 14:54:47 -0800

    adding layout.css file

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,24 @@
 import React from "react"
-
-export default ({ children }) => (
-  <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
-    {children}
-  </div>
-)
+import { Link } from "gatsby"
+import "./layout.css"
+const ListLink = props => (
+    <li className="listLink">
+      <Link to={props.to}>{props.children}</Link>
+    </li>
+  )
+  
+  export default ({ children }) => (
+    <div className="main">
+      <header className="headerStyle">
+        <Link to="/">
+          <h3 className="headingStyle">MySweetSite</h3>
+        </Link>
+        <ul className="listStyle">
+          <ListLink to="/">Home</ListLink>
+          <ListLink to="/about/">About</ListLink>
+          <ListLink to="/contact/">Contact</ListLink>
+        </ul>
+      </header>
+      {children}
+    </div>
+  )
